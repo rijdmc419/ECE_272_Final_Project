1b1b0632d3b153e2a52b19a4c6e57cf683ea71b4
Ethan Burton <burtonet@oregonstate.edu>
2022-11-30 12:51:01 -0800

    tried making a comparator/counter frequency generator, didn't quite work.

--- comparator.sv
+++ comparator.sv
@@ -1,12 +1,13 @@
-module comparator #(parameter N = 8, b = 8)
-							(input logic [N-1:0] a, b,
+module comparator #(parameter N = 8)
+							(input logic [N-1:0] a,
+							 input logic [10:0]c,
 							output logic eq, neq, lt, lte, gt, gte);
 							
 							
-			assign eq = (a == b);
-			assign neq = (a != b);
-			assign lt = (a < b);
-			assign lte = (a <= b);
-			assign gt = (a > b);
-			assign gte = (a >= b);
+			assign eq = (a == c);
+			assign neq = (a != c);
+			assign lt = (a < c);
+			assign lte = (a <= c);
+			assign gt = (a > c);
+			assign gte = (a >= c);
 endmodule
