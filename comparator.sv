1481a091aa3a9b63c66d623d151266362bbf152a
Ethan Burton <burtonet@oregonstate.edu>
2022-12-01 15:58:54 -0800

    Frequency generator making noise, but wrong pitch

--- comparator.sv
+++ comparator.sv
@@ -1,13 +1,13 @@
-module comparator #(parameter N = 8)
+module comparator #(parameter N = 8, M = 17, F = 2)
 							(input logic [N-1:0] a,
-							 input logic [10:0]c,
+							 input logic [M-1:0]c,
 							output logic eq, neq, lt, lte, gt, gte);
 							
 							
 			assign eq = (a == c);
 			assign neq = (a != c);
 			assign lt = (a < c);
-			assign lte = (a <= c);
+			assign lte = (a <= F*c);
 			assign gt = (a > c);
-			assign gte = (a >= c);
+			assign gte = (a >= F*c);
 endmodule
